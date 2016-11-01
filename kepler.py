48eca764cda0a10814c2fc44edb69f0bbc0f588b
Sven Prüfer <pruefer.sven@gmail.com>
2016-11-01 15:02:04 +0100

    kepler.py: Add method export_position_velocity to return position and velocity in inertial system

--- kepler.py
+++ kepler.py
@@ -39,6 +39,7 @@ class celestial_body:
         self.argument_periapsis = argument_periapsis
         self.true_anomaly_epoch = true_anomaly_epoch
         self.mass = mass
+        self.parameter = semi_major_axis * (1 - eccentricity**2)
     
     @classmethod
     def from_position_velocity(self,mass,position,velocity):
@@ -60,11 +61,34 @@ class celestial_body:
         
         body = celestial_body(mass,semi_major_axis,eccentricity,inclination,longitude_ascending_node,argument_periapsis,true_anomaly_epoch)
         return body
+    
+    def export_position_velocity(self):
+        # Exports position and velocity of celestial body. How should time dependence be incorparated? Should it be a parameter for this function?
+        r = self.parameter  / ( 1 + self.eccentricity * np.cos(self.true_anomaly_epoch))
+
+        # The perifocal coordinate system uses coordinate axes P, Q, W in this order, where P points in the direction of the periapsis and Q is perpendicular in positive direction in the plane of the orbit.
+        position_perifocal_system = np.array([r * np.cos(self.true_anomaly_epoch),r * np.sin(self.true_anomaly_epoch),0],float)
+        velocity_perifocal_system = np.sqrt(mu / self.parameter) * np.array([-np.sin(self.true_anomaly_epoch),self.eccentricity + np.cos(self.true_anomaly_epoch),0],float)
+        
+        # Calculate the rotation matrix from perifocal to fixed frame. Bate says, one should avoid this technique.
+        rotation_matrix = np.array([[np.cos(self.longitude_ascending_node) * np.cos(self.argument_periapsis) - np.sin(self.longitude_ascending_node) * np.sin(self.argument_periapsis) * np.cos(self.inclination) , - np.cos(self.longitude_ascending_node) * np.sin(self.argument_periapsis) - np.sin(self.longitude_ascending_node) * np.cos(self.argument_periapsis) * np.cos(self.inclination) , np.sin(self.longitude_ascending_node) * np.sin(self.inclination)],\
+        [np.sin(self.longitude_ascending_node) * np.cos(self.argument_periapsis) + np.cos(self.longitude_ascending_node) * np.sin(self.argument_periapsis) * np.cos(self.inclination) , - np.sin(self.longitude_ascending_node) * np.sin(self.argument_periapsis) + np.cos(self.longitude_ascending_node) * np.cos(self.argument_periapsis) * np.cos(self.inclination) , - np.cos(self.longitude_ascending_node) * np.sin(self.inclination)],\
+        [ np.sin(self.argument_periapsis) * np.sin(self.inclination) , np.cos(self.argument_periapsis) * np.sin(self.inclination) , np.cos(self.inclination)]\
+        ],float)
+        
+        position = np.dot(rotation_matrix,position_perifocal_system)
+        velocity = np.dot(rotation_matrix,velocity_perifocal_system)
+        
+        return position, velocity
+
+#############
+# Test Code #
+#############
 
 position = np.array([3.0 / 4 * np.sqrt(3), 3.0 /4, 0],float)
 velocity = np.array([-1.0/(2*np.sqrt(2)), np.sqrt(3) / (2 * np.sqrt(2)), 1.0 / np.sqrt(2)],float) 
 test_body = celestial_body.from_position_velocity(1,position,velocity)
-print(test_body.inclination)
+print(test_body.export_position_velocity())
 
 
 
