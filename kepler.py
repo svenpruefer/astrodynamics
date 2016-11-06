a070cd7d7937ed488b75ae53b35e2ebe6ec97d2c
Sven Prüfer <pruefer.sven@gmail.com>
2016-11-06 21:44:47 +0100

    kepler.py: Add quadrant check and plot of orbit

--- kepler.py
+++ kepler.py
@@ -4,6 +4,15 @@
 
 import numpy as np
 from scipy.optimize import fsolve
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+############################
+# Define Figure parameters #
+############################
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
 
 #####################
 # Define parameters #
@@ -43,9 +52,13 @@ class celestial_body:
         self.true_anomaly_epoch = true_anomaly_epoch # nu
         self.mass = mass # m
         self.parameter = semi_major_axis * (1 - eccentricity**2) # p
-        self.eccentric_anomaly = np.arccos((self.eccentricity + np.cos(self.true_anomaly_epoch)) / (1 + self.eccentricity * np.cos(self.true_anomaly_epoch))) # E
+        if ( 0 <= self.true_anomaly_epoch ) and ( self.true_anomaly_epoch <= np.pi):
+            self.eccentric_anomaly = np.arccos((self.eccentricity + np.cos(self.true_anomaly_epoch)) / (1 + self.eccentricity * np.cos(self.true_anomaly_epoch))) # E, at the moment the cases dont't cover everything.
+        else:
+            self.eccentric_anomaly = 2 * np.pi - np.arccos((self.eccentricity + np.cos(self.true_anomaly_epoch)) / (1 + self.eccentricity * np.cos(self.true_anomaly_epoch))) # E
         self.mean_anomaly = self.eccentric_anomaly - self.eccentricity * np.sin(self.eccentric_anomaly) # M
-        self.mean_motion = np.sqrt(mu / self.semi_major_axis**3 )# n
+        self.mean_motion = np.sqrt(mu / self.semi_major_axis**3 ) # n
+        self.period = 2 * np.pi / np.sqrt(mu) * np.sqrt(self.semi_major_axis**3) # T
     
     @classmethod
     def from_position_velocity(self,mass,position,velocity):
@@ -61,9 +74,18 @@ class celestial_body:
         semi_major_axis = p / (1-np.dot(e,e))
         eccentricity = norm(e)
         inclination = np.arccos(h[2] / norm(h))
-        longitude_ascending_node = np.arccos(n[0] / norm(n))
-        argument_periapsis = np.arccos(np.dot(n,e) / (norm(n) * norm(e)))
-        true_anomaly_epoch = np.arccos(np.dot(e,position) / (norm(e) * norm(position)))
+        if position[1] >= 0:
+            longitude_ascending_node = np.arccos(n[0] / norm(n))
+        else:
+            longitude_ascending_node = 2 * np.pi - np.arccos(n[0] / norm(n))
+        if e[2] >= 0:
+            argument_periapsis = np.arccos(np.dot(n,e) / (norm(n) * norm(e)))
+        else:
+            argument_periapsis = 2 * np.pi - np.arccos(np.dot(n,e) / (norm(n) * norm(e)))
+        if np.dot(position,velocity) >= 0:
+            true_anomaly_epoch = np.arccos(np.dot(e,position) / (norm(e) * norm(position)))
+        else:
+            true_anomaly_epoch = 2 * np.pi - np.arccos(np.dot(e,position) / (norm(e) * norm(position)))
         
         body = celestial_body(mass,semi_major_axis,eccentricity,inclination,longitude_ascending_node,argument_periapsis,true_anomaly_epoch)
         return body
@@ -98,7 +120,10 @@ class celestial_body:
         new_eccentric_anomaly = fsolve(lambda E : E - self.eccentricity * np.sin(E) -new_mean_anomaly,new_mean_anomaly) 
         
         # Calculate new true anomaly at epoch
-        new_true_anomaly_epoch = np.arccos( ( np.cos(new_eccentric_anomaly) - self.eccentricity ) / ( 1 - self.eccentricity * np.cos(new_eccentric_anomaly)))
+        if new_eccentric_anomaly <= np.pi:
+            new_true_anomaly_epoch = np.arccos( ( np.cos(new_eccentric_anomaly) - self.eccentricity ) / ( 1 - self.eccentricity * np.cos(new_eccentric_anomaly)))
+        else:
+            new_true_anomaly_epoch = 2 * np.pi - np.arccos( ( np.cos(new_eccentric_anomaly) - self.eccentricity ) / ( 1 - self.eccentricity * np.cos(new_eccentric_anomaly)))
         
         # Update values of true anomaly at epoch and eccentric anomaly and mean anomaly
         self.true_anomaly_epoch = new_true_anomaly_epoch
@@ -112,12 +137,29 @@ class celestial_body:
 position = np.array([3.0 / 4 * np.sqrt(3), 3.0 /4, 0],float)
 velocity = np.array([-1.0/(2*np.sqrt(2)), np.sqrt(3) / (2 * np.sqrt(2)), 1.0 / np.sqrt(2)],float) 
 test_body = celestial_body.from_position_velocity(1,position,velocity)
-output = test_body.export_position_velocity()[0]
-print(output)
-test_body.advance_in_time(0.1)
-print(test_body.export_position_velocity()[0])
 
+#output = test_body.export_position_velocity()[0][0]
+#print(output)
+#test_body.advance_in_time(0.1)
+#print(test_body.export_position_velocity()[0])
 
+X, Y, Z = [], [], []
+T = test_body.period
+print(T)
+
+for i in range(1000):
+    X.append(test_body.export_position_velocity()[0][0])
+    Y.append(test_body.export_position_velocity()[0][1])
+    Z.append(test_body.export_position_velocity()[0][2])
+    test_body.advance_in_time(T / 2000.0)
+
+#print(Z[450])
+
+ax.scatter(np.array([0]),np.array([0]),np.array([0]))
+ax.plot(np.array(X),np.array(Y),np.array(Z))
+#plt.zlim(-5,5)
+ax.set_zlim(-5.0,5.0)
+plt.show()
 
 
 
