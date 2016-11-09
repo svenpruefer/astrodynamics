1e1b9c7e83bbe96478c659e238e7f512b62b4064
svenpruefer <pruefer.sven@gmail.com>
2016-11-10 00:24:47 +0100

    Fixed Kepler two-body-calculations.

--- kepler.py
+++ kepler.py
@@ -16,22 +16,32 @@ def kepler_problem(mass_1,position_1,velocity_1,mass_2,position_2,velocity_2):
     
     center_of_mass = ( mass_1 * position_1 + mass_2 * position_2 ) / ( mass_1 + mass_2 )
     center_of_mass_velocity = ( mass_1 * velocity_1 + mass_2 * velocity_2 ) / ( mass_1 + mass_2 )
-    mu = 1 * ( mass_1 + mass_2 ) # Gravitation constant = 1
+    mu = 1.0 * ( mass_1 + mass_2 ) # Gravitation constant = 1
     
-    position_1_in_center_of_mass_system = position_1 - center_of_mass
-    position_2_in_center_of_mass_system = position_2 - center_of_mass
+    relative_position = position_1 - position_2
+    relative_velocity = velocity_1 - velocity_2
     
-    velocity_1_in_center_of_mass_system = velocity_1 - center_of_mass_velocity
-    velocity_2_in_center_of_mass_system = velocity_2 - center_of_mass_velocity
+    total_body = celestial_body.from_position_velocity(mass_1,mu,relative_position,relative_velocity)
     
-    print(position_1_in_center_of_mass_system)
-    print(position_2_in_center_of_mass_system)
-    print(velocity_1_in_center_of_mass_system)
-    print(velocity_2_in_center_of_mass_system)
+    #position_1_in_center_of_mass_system = mu / mass_1 * total_body.export_position_velocity()[0]
+    #position_2_in_center_of_mass_system = - mu / mass_2 * total_body.export_position_velocity()[0]
     
-    body_1 = celestial_body.from_position_velocity(mass_1,mu,position_1_in_center_of_mass_system,velocity_1_in_center_of_mass_system)
-    body_2 = celestial_body.from_position_velocity(mass_2,mu,position_2_in_center_of_mass_system,velocity_2_in_center_of_mass_system)
+    #velocity_1_in_center_of_mass_system = mu / mass_1 * total_body.export_position_velocity()[1]
+    #velocity_2_in_center_of_mass_system = - mu / mass_2 * total_body.export_position_velocity()[1]
     
-    return body_1, body_2  
+    #position_1_in_center_of_mass_system = position_1 - center_of_mass #mu / mass_1 * relative_position
+    #position_2_in_center_of_mass_system = position_2 - center_of_mass #- mu / mass_2 * relative_position
+    #velocity_1_in_center_of_mass_system = velocity_1 - center_of_mass_velocity
+    #velocity_2_in_center_of_mass_system = velocity_2 - center_of_mass_velocity
+    
+    #print(position_1_in_center_of_mass_system)
+    #print(velocity_1_in_center_of_mass_system)
+    #print(position_2_in_center_of_mass_system)
+    #print(velocity_2_in_center_of_mass_system)
+    
+    #body_1 = celestial_body.from_position_velocity(mass_1,mu,position_1_in_center_of_mass_system,velocity_1_in_center_of_mass_system)
+    #body_2 = celestial_body.from_position_velocity(mass_2,mu,position_2_in_center_of_mass_system,velocity_2_in_center_of_mass_system)
+    
+    return total_body#body_1, body_2  
     
 
--- test.py
+++ test.py
@@ -26,27 +26,42 @@ mu = 1.0 # Gravitational Parameter, equal to product of gravitational constant a
 # Define Objects #
 ###################
 
-position_1 = np.array([3.0 / 4 * np.sqrt(3), 3.0 /4, 0],float)
-velocity_1 = np.array([-1.0/(2*np.sqrt(2)), np.sqrt(3) / (1.6 * np.sqrt(2)), 1.0 / np.sqrt(2)],float) 
-test_body_1 = celestial_body.from_position_velocity(1,mu,position_1,velocity_1)
+position_1 = np.array([3.0 / 4 * np.sqrt(3), 3.0 /4, -0.1],float)
+velocity_1 = np.array([-1.0/(2*np.sqrt(2)), np.sqrt(3) / (1.6 * np.sqrt(2)), 1.0 / np.sqrt(2)],float)
+mass_1 = 2
+#test_body_1 = celestial_body.from_position_velocity(1,mu,position_1,velocity_1)
 
-position_2 = np.array([2.5 / 4 * np.sqrt(3), 2.9 /4, 0.1],float)
-velocity_2 = np.array([0.7/(2*np.sqrt(2)), -np.sqrt(3) / (2.4 * np.sqrt(2)), -1.0 / np.sqrt(2)],float) 
-test_body_2 = celestial_body.from_position_velocity(1,mu,position_2,velocity_2)
+position_2 = np.array([2.5 / 4 * np.sqrt(3), 2.9 /4, 0.3],float)
+velocity_2 = np.array([0.7/(2*np.sqrt(2)), -np.sqrt(3) / (2.4 * np.sqrt(2)), -1.0 / np.sqrt(2)],float)
+mass_2 = 1
 
-body_1, body_2 = kepler_problem(1,position_1,velocity_1,1,position_2,velocity_2)
+mu = 1 * (mass_1 + mass_2)
+#test_body_2 = celestial_body.from_position_velocity(1,mu,position_2,velocity_2)
+
+#body_1, body_2 = kepler_problem(1,position_1,velocity_1,1,position_2,velocity_2)
+
+total_body = kepler_problem(mass_1,position_1,velocity_1,mass_2,position_2,velocity_2)
 
 #############
 # Plotting #
 #############
 
 
-position1 = body_1.export_orbit(100)
-position2 = body_2.export_orbit(100)
+#position1 = body_1.export_orbit(100)
+#position2 = body_2.export_orbit(100)
+
+position1 = mu / mass_1 * total_body.export_orbit(100)
+position2 = - mu / mass_2 * total_body.export_orbit(100)
+
+#lines = np.vstack( (position1[20,:],position2[20,:]) )
+
+#print(position1)
+#print(lines)
 
 ax.scatter(np.array([0]),np.array([0]),np.array([0]))
 ax.plot(position1[:,0],position1[:,1],position1[:,2])
 ax.plot(position2[:,0],position2[:,1],position2[:,2])
+#ax.plot(lines[:,0],lines[:,1],lines[:,2])
 
 ax.set_zlim(-5.0,5.0)
 plt.show()
